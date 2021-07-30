bfa606bd3ed56d992c94216e7d668c4df4ff2961
PietroDS1 <danton.pietro@gmail.com>
2021-07-30 15:40:19 -0300

    Spring Agora Vai

---
+++ Spring/MinhaLojaDeGames/src/main/java/com/Exercicio/MinhaLojaDeGames/MinhaLojaDeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.Exercicio.MinhaLojaDeGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojaDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaLojaDeGamesApplication.class, args);
+	}
+
+}
